39e1d940d60bfdf450bac5a3bd6f1c42ddd9e2c9
Ritsu_idk <karensantiago071104@gmail.com>
2025-07-08 16:56:31 -0600

    1.0.3
    si

--- Quantum/src/App.tsx
+++ Quantum/src/App.tsx
@@ -1,11 +1,17 @@
 import './App.css'
+import Layout from './components/Layout';
+
 function App() {
 
   return (
-    <>
+    <div>
+      
+    <h1>Quantum wazaaaaaaaa</h1>
+    <Layout />
+    
 
-    </>
+    </div>
   )
 }
 
-export default App
+export default App;
--- Quantum/src/components/Layout.tsx
+++ Quantum/src/components/Layout.tsx
@@ -3,29 +3,18 @@ import { Outlet,Link } from 'react-router-dom';
 
 const Layout = () => {
     return(
-        <div>
-            <nav>
-                <ul>
-                    <li>
+        <div className='bg-gray-800 text-blue  p-4 shadow-md inline-bl'>
+            <nav>  
                         <Link to="/">Home</Link>
-                    </li>
-                    <li>
-                        <Link to="/servicios">Servicios</Link>
-                    </li>
-                    <li>
-                        <Link to="/proyectos">Proyectos</Link>
-                    </li>
-                    <li>
+                        <Link to="/servicio">Servicios</Link>
+                        <Link to="/proyecto">Proyectos</Link>
                         <Link to="/quantum_360">Quantum 360°</Link>
-                    </li>
-                    <li>
                         <Link to="/contacto">Contacto</Link>
-                    </li>
-                </ul>
             </nav>
             <Outlet/>
 
         </div>
+
 
 );
 }
