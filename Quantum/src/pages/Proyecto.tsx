aea078270f7685a90bfe57e49052dddeb011a937
Marco Antonio Rulfo Castro <81320236+Marcor360@users.noreply.github.com>
2025-07-22 18:11:16 -0600

    1.18.16

--- Quantum/src/pages/Proyecto.tsx
+++ Quantum/src/pages/Proyecto.tsx
@@ -299,11 +299,12 @@ const Proyecto: React.FC = () => {
           }`}
       >
         <section className="py-16">
-          <div className="max-w-2xl mx-auto px-4 sm:px-6 lg:px-7">
+          <div className="max-w-full mx-auto px-2 sm:px-6 lg:px-7">
             <Carousel />
           </div>
         </section>
       </div>
+
     </main>
   );
 };
