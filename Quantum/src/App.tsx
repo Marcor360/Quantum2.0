ef7ed9197e552707abadc29bd175220fb78ae1f6
Marco Antonio Rulfo Castro <81320236+Marcor360@users.noreply.github.com>
2025-12-12 10:39:36 -0600

    3.1.0
    Característica: Integración de React Router y Tailwind CSS, añadir página de inicio
    
    - Se actualizó package.json para incluir Tailwind CSS, GSAP, React Router y otras dependencias.
    - Se modificó App.tsx para configurar el enrutamiento con un componente de inicio.
    - Se añadió la importación de Tailwind CSS a index.css.
    - Se envolvió el componente App con BrowserRouter en main.tsx para compatibilidad con el enrutamiento.
    - Se configuró Vite para usar plugins Tailwind CSS y SVGR.
    - Se creó un nuevo componente de inicio con un mensaje de bienvenida.

--- Quantum/src/App.tsx
+++ Quantum/src/App.tsx
@@ -1,12 +1,15 @@
-
 import './App.css'
+import { Routes, Route } from 'react-router-dom'
 
+{/*Importacion de Paginas*/ }
+import Home from './assets/pages/Home'
 function App() {
 
-
   return (
-    
-  
+    <Routes>
+      <Route path='/' element={<Home />} />
+      <Route index element={<Home />} />
+    </Routes>
   )
 }
 
