fe6558c28d983977ccdc63289453748a9a27a51b
Marco Antonio Rulfo Castro <81320236+Marcor360@users.noreply.github.com>
2026-01-28 17:30:02 -0600

    4.9.4

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,11 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import { BrowserRouter } from 'react-router-dom'
+import ScrollToTop from './components/ScrollToTop'
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
+      <ScrollToTop />
       <App />
     </BrowserRouter>
   </StrictMode>,
