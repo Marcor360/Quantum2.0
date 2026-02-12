b482b0c1e62a187ffd8e7196a5719f6943a65c75
Marco Antonio Rulfo Castro <81320236+Marcor360@users.noreply.github.com>
2026-02-12 13:44:23 -0600

    4.21.1

--- src/pages/servicios/campañas.tsx
+++ src/pages/servicios/campañas.tsx
@@ -1,6 +1,13 @@
+import Head from "../../components/Header";
+import Footer from "../../components/Footer";
+import "./campañas.css"
+
 export default function Campañas() {
     return (
         <>
+            <Head />
+
+            <Footer />
         </>
     )
 }
